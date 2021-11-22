db6a7f56a6b2cc00ace8ed36e4136229d776116e
Denis Bystruev <dbystruev@me.com>
2021-11-22 18:15:13 +0300

    Created 2 image view with tap gesture recognisers

--- Image View Tap/ViewController.swift
+++ Image View Tap/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var folderImageView: UIImageView!
+    @IBOutlet weak var escapeImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for imageView in [escapeImageView, folderImageView] {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
+            imageView?.addGestureRecognizer(tapGesture)
+            imageView?.isUserInteractionEnabled = true
+        }
+        
     }
 
+    @objc func imageTapped(_ sender: Any) {
+        guard let gesture = sender as? UITapGestureRecognizer else {
+            print(#line, "ERROR: \(sender) is not \(UITapGestureRecognizer.self)")
+            return
+        }
+        
+        guard let imageView = gesture.view as? UIImageView else {
+            print(#line, "ERROR: \(String(describing: gesture.view)) is not \(UIImageView.self)")
+            return
+        }
+        
+        print(#line, "\(String(describing: imageView.image))")
+    }
 
 }
 
